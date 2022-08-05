e64d634cd4d1445b50a5fcfba5da1cf723c00a49
Ken1567 <107854367+Ken1567@users.noreply.github.com>
2022-08-05 11:24:42 +0800

    Update capstone_query.sql

--- capstone_query.sql
+++ capstone_query.sql
@@ -2,55 +2,45 @@ CREATE DATABASE express;
 
 CREATE TABLE users(
     users_id serial PRIMARY KEY NOT NULL,
-    user_username VARCHAR(50) NOT NULL,
-    user_password VARCHAR(50) NOT NULL,
-    user_pen_name VARCHAR(50) UNIQUE NOT NULL,
-    user_first_name VARCHAR(50) NOT NULL,
-    user_last_name VARCHAR(50) NOT NULL,
-    user_birth_date VARCHAR(50) NOT NULL,
-    user_email_address VARCHAR(50) NOT NULL
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    pen_name VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_date VARCHAR(50) NOT NULL,
+    email_address VARCHAR(50) NOT NULL,
+    bio VARCHAR(50) NOT NULL,
+    link VARCHAR(50) NOT NULL,
+    following INT,
+    followers INT
 );
 
-INSERT INTO users(users_id, user_username, user_password, user_pen_name, user_first_name, user_last_name, user_birth_date, user_email_address)
-VALUES (1, '@shinmentakezo12', 'Password123', 'Bard of Sorrows', 'John', 'Smith', 'July 1995', 'johnsmith@gmail.com');
+INSERT INTO users(users_id, username, password, pen_name, first_name, last_name, birth_date, email_address, bio, link, following, followers)
+VALUES (1, '@shinmentakezo12', 'Password123', 'Bard of Sorrows', 'John', 'Smith', 'July 1995', 'johnsmith@gmail.com', 'Male. Programmer. Edgy.', 'github/Ken1567', 1256, 1234);
 
-CREATE TABLE log_in(
-    log_in_id serial PRIMARY KEY NOT NULL,
-    user_username VARCHAR(20) NOT NULL,
-    user_password VARCHAR(20) NOT NULL
+CREATE TABLE post(
+    post_id serial PRIMARY KEY NOT NULL,
+    pen_name VARCHAR(20) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    time_stamp DATE NOT NULL,
+    post_message TEXT NOT NULL,
+    privacy BOOLEAN NOT NULL
 );
 
-INSERT INTO log_in(log_in_id, user_username, user_password)
-VALUES (1, '@shinmentakezo12', 'Password123');
+INSERT INTO post(post_id, pen_name, username, time_stamp, post_message, privacy)
+VALUES (1, 'Bard of Sorrows', '@shinmentakezo12', '1997-12-01', 'To love and to be loved is to feel the sun from both sides', True);
 
-CREATE TABLE homepage(
-    homepage_id serial PRIMARY KEY NOT NULL,
-    user_pen_name VARCHAR(20) NOT NULL,
-    user_username VARCHAR(20) NOT NULL,
-    homepage_time_stamp DATE NOT NULL,
-    homepage_message TEXT NOT NULL,
-    homepage_privacy VARCHAR(15) NOT NULL
+CREATE TABLE comment(
+    comment_id serial PRIMARY KEY NOT NULL,
+    pen_name VARCHAR(20) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    comment_message TEXT NOT NULL
 );
 
-INSERT INTO homepage(homepage_id, user_pen_name, user_username, homepage_time_stamp, homepage_message, homepage_privacy)
-VALUES (1, 'Bard of Sorrows', '@shinmentakezo12', '1997-12-01', 'Hello', 'Public');
+INSERT INTO comment(comment_id, pen_name, username, comment_message)
+VALUES (1, 'Merchant of Venice', '@sasakikojiro15', 'But burns twice as much.');
 
-CREATE TABLE profiles(
-    profile_id serial PRIMARY KEY NOT NULL,
-    user_pen_name VARCHAR(20) NOT NULL,
-    user_username VARCHAR(20) NOT NULL,
-    profile_bio TEXT NOT NULL,
-    profile_link VARCHAR(50) NOT NULL,
-    profile_following INT,
-    profile_followers INT
-);
-
-INSERT INTO profiles(profile_id, user_pen_name, user_username, profile_bio, profile_link, profile_following, profile_followers)
-VALUES (1, 'Bard of Sorrows', '@shinmentakezo12', 'Male. Programmer. Edgy.', 'github/Ken1567', 1256, 1234);
-
-SELECT log_in.*, homepage.*, profiles.*
-FROM log_in
-INNER JOIN homepage
-ON log_in.log_in_id = homepage.homepage_id
-INNER JOIN profiles
-ON log_in.log_in_id = profiles.profile_id
+SELECT post.*, comment.*
+FROM post
+INNER JOIN comment
+ON post.post_id = comment.comment_id
